4f6be479e1820fd575f330be9a0161c2e26813bb
Alexandre Dewagnier <dewagnier@gmail.com>
2021-03-21 15:11:49 +0100

    Update Unity and Wwise

--- UnityProject/Assets/Scripts/WwiseManager.cs
+++ UnityProject/Assets/Scripts/WwiseManager.cs
@@ -6,8 +6,6 @@ public class WwiseManager : MonoBehaviour
 {
     private void Awake()
     {
-        uint bankID; // Not used
-        AkSoundEngine.LoadBank("Soundbank", AkSoundEngine.AK_DEFAULT_POOL_ID, out bankID);
     }
 
     // Start is called before the first frame update
@@ -15,7 +13,7 @@ public class WwiseManager : MonoBehaviour
     {
         AkSoundEngine.PostEvent("Play_music", gameObject);
         AkSoundEngine.PostEvent("ambiance", gameObject);
-        //AkSoundEngine.SetState("game_state", "game1A");
+        AkSoundEngine.SetState("game_state", "game1A");
     }
 
     // Update is called once per frame
